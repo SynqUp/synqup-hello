120dc331db407ff4df81738498ecd0be5515e8e1
y-nizan <155479807+y-nizan@users.noreply.github.com>
2025-08-13 12:55:34 +0300

    Add files via upload

---
+++ function-file.js
@@ -0,0 +1,15 @@
+/* global Office */
+Office.onReady(() => {});
+
+Office.actions.associate('onAppointmentSendHandler', onAppointmentSendHandler);
+
+async function onAppointmentSendHandler(event) {
+  try {
+    event.completed({
+      allowEvent: false,
+      errorMessage: 'Hello from SynqUp - add-in is installed'
+    });
+  } catch (e) {
+    event.completed({ allowEvent: true });
+  }
+}
